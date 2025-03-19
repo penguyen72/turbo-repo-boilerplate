924ce215e4e293762be3032d5e735606b15c0b55
penguyen72 <peytonn72@gmail.com>
2025-03-19 16:25:14 -0700

    update ui

--- frontend/web/src/App.tsx
+++ frontend/web/src/App.tsx
@@ -2,14 +2,22 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 import './App.css';
 
+const DEFAULT_CARS: any[] = [];
+
 function App() {
   return (
-    <div className="grid grid-cols-2 h-full bg-gray-100 gap-1">
-      <div className="flex flex-col bg-white items-center">
-        <p className="text-2xl font-bold my-12">Sign In</p>
+    <div className="grid h-full grid-cols-2 gap-1 bg-gray-100">
+      <div className="flex flex-col items-center bg-red-600">
+        <div className="flex flex-col items-center gap-4 my-auto">
+          <p className="text-2xl font-bold">Sign In</p>
+          <input className="px-4 py-2 border rounded-md" placeholder="Username" type="text" />
+          <input className="px-4 py-2 border rounded-md" placeholder="Password" type="password" />
+          <button className="px-4 py-2 bg-white border rounded-md">Sign In</button>
+        </div>
       </div>
-      <div className="flex flex-col bg-white items-center">
-        <p className="text-2xl font-bold my-12">Cars</p>
+      <div className="flex flex-col items-center bg-white">
+        <p className="my-12 text-2xl font-bold">Cars</p>
+        <pre>{JSON.stringify(DEFAULT_CARS, null, 4)}</pre>
       </div>
     </div>
   );
