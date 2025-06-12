efa9a94e1bf7b92ce666de7feddb3d20045d44a2
Joshua Goon <jgoon@tesla.com>
2025-06-12 11:11:28 -0700

    update frontend page

--- frontend/web/src/App.tsx
+++ frontend/web/src/App.tsx
@@ -1,24 +1,14 @@
-import axios from 'axios';
-import Cookies from 'js-cookie';
 import './App.css';
 
-const DEFAULT_CARS: any[] = [];
 const API_BASE = 'http://localhost:8000/api';
 
 function App() {
   return (
-    <div className="grid h-full grid-cols-2 gap-1 bg-gray-100">
-      <div className="flex flex-col items-center bg-red-600">
-        <div className="flex flex-col items-center gap-4 my-auto">
-          <p className="text-2xl font-bold">Sign In</p>
-          <input className="px-4 py-2 border rounded-md" placeholder="Username" type="text" name="username" />
-          <input className="px-4 py-2 border rounded-md" placeholder="Password" type="password" name="password" />
-          <button className="px-4 py-2 bg-white border rounded-md">Sign In</button>
-        </div>
-      </div>
-      <div className="flex flex-col items-center bg-white">
-        <p className="my-12 text-2xl font-bold">Cars</p>
-        <pre>{JSON.stringify(DEFAULT_CARS, null, 4)}</pre>
+    <div className="h-full flex items-center justify-center">
+      <div className="w-96 h-96 flex flex-col gap-4 bg-white shadow-lg rounded-lg p-6">
+        <h1 className="font-bold text-2xl">todos</h1>
+
+        <div>list todos here</div>
       </div>
     </div>
   );
